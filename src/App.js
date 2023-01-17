22c9899f93d9ab2f16d2378f10312a2a738c3795
aswinina <116088497+aswinina@users.noreply.github.com>
2023-01-17 13:14:06 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by<a href="https://aswinisureddy-profile.netlify.app/" target="_blank" >Aswini Sureddy </a>and is {""}
+      <a href="https://github.com/aswinina/react-weather-app" target="_blank">open sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
