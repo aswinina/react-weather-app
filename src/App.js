5cbd71786314954c9a2cff70c99d87656676062a
aswinina <116088497+aswinina@users.noreply.github.com>
2023-01-18 12:31:54 +0100

    added basic design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-
+import React from "react";
+import Weather from "./weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div classNmae="container">
+        <Weather />
       <footer>
-        This project was coded by<a href="https://aswinisureddy-profile.netlify.app/" target="_blank" >Aswini Sureddy </a>and is {""}
+        This project was coded by{" "}<a href="https://aswinisureddy-profile.netlify.app/" target="_blank" >Aswini Sureddy </a>{" "}and is {""}
       <a href="https://github.com/aswinina/react-weather-app" target="_blank">open sourced on GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
