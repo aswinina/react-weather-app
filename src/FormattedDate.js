833db462df5f877aa4ab6ea2a7a85f95782716c1
aswinina <116088497+aswinina@users.noreply.github.com>
2023-01-19 11:24:52 +0100

    Added date on the weather component

---
+++ src/FormattedDate.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function FormattedDate(props){
+    console.log(props.date);
+    let days=["sunday","monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day=days[props.data.getDay()];
+    let hours=props.data.getHours();
+    if(hours < 10){
+        hours = `0{hours}`;
+    }
+let minutes = props.date.getMinutes();
+if(minutes < 10){
+    minutes = `0{mintutes}`;
+}
+return(<div>{day} {hours}:{minutes}</div>);
+}
